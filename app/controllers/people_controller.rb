005f64e9965624a45abb2dbfd5087ace2c51933a
asanon07 <sakuse0727@outlook.com>
2021-03-02 03:23:09 +0900

    people-add

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,4 +3,21 @@ class PeopleController < ApplicationController
     @msg = "Person data."
     @data = Person.all
   end
+
+  def add
+    @person = Person.new
+  end
+
+  def create
+    if request.post?
+      Person.create(person_params)
+    end
+    redirect_to "/people"
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:name, :age, :email)
+  end
 end
