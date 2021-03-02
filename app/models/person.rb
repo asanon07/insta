6f63083c14f75deb445882093545c4c41388ab26
asanon07 <sakuse0727@outlook.com>
2021-03-03 00:59:24 +0900

    meke-validate

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,11 +1,13 @@
 class Person < ApplicationRecord
   validates :name, presence: true
+
   validates :age, presence: { message: "を入力してください" }
   validates :age, length: { maximum: 3 }
+  validates :age, numericality: true
 
   validates :email, presence: { message: "を入力してください" }
-  validates :password, presence: { message: "を入力してください" }
-
-  validates :age, numericality: true
   validates :email, uniqueness: true
+
+  validates :password, presence: { message: "を入力してください" }
+  validates :password, uniqueness: true
 end
