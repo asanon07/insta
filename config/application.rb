69c3832e883cc0c1b4b91012e425cd728f63e4f1
asanon07 <sakuse0727@outlook.com>
2021-03-03 00:55:23 +0900

    remake-people-table

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,8 +9,9 @@ Bundler.require(*Rails.groups)
 module Insta
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    I18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
+    config.i18n.default_locale = :ja
     config.load_defaults 5.2
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Person.create(name: "テスト太郎5", authority: 1, registration_date: "2020", registration_quantity: 1, login_date: "20200102", user_id: 1)
+# Person.create(name: "テスト太郎5", authority: 1, registration_date: "2020", registration_quantity: 1, login_date: "20200102", user_id: 1)
 
 # Person.create(name: "テスト太郎4", authority: 2, registration_date: "20200202", registration_quantity: 2, login_date: "20200212", user_id: 2)
