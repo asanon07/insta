67ad4de5396b4e4e074396cc19f42bf87ab47e6b
asanon07 <sakuse0727@outlook.com>
2021-03-08 10:15:46 +0900

    remake-location-model

--- db/migrate/20210308005237_create_locations.rb
+++ db/migrate/20210308005237_create_locations.rb
@@ -2,11 +2,12 @@ class CreateLocations < ActiveRecord::Migration[5.2]
   def change
     create_table :locations do |t|
       t.text :name
+      t.text :prefecture
       t.text :municipalities
       t.text :address
       t.integer :division
       t.string :photo
-      t.text :registration_date
+      t.text :photo_description
       t.integer :user_id
 
       t.timestamps
