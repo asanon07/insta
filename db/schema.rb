a37bc0cde97f3240db0a4f1b660a71a5c95927f2
asanon07 <sakuse0727@outlook.com>
2021-03-08 10:15:46 +0900

    remake-location-model

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_052800) do
+ActiveRecord::Schema.define(version: 2021_03_08_005237) do
 
   create_table "locations", force: :cascade do |t|
     t.text "name"
+    t.text "prefecture"
     t.text "municipalities"
     t.text "address"
     t.integer "division"
     t.string "photo"
-    t.text "registration_date"
+    t.text "photo_description"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
