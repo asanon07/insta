0ba8ef75ac4c589ae4f2e86ec701e36a5bf14cd6
asanon07 <67551508+asanon07@users.noreply.github.com>
2021-04-07 01:36:23 +0900

    add-location-validate

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,11 @@
 class Location < ApplicationRecord
   mount_uploader :photo, PhotoUploader
+
+  validates :name, presence: true
+  validates :prefecture, presence: true
+  validates :municipalities, presence: true
+  validates :address, presence: true
+  validates :photo, presence: true
+  validates :photo_description, presence: true
+
 end
