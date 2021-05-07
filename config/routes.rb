6e931199886609a06638641dc0bdfeb6021198bf
asanon07 <sakuse0727@outlook.com>
2021-05-08 02:18:16 +0900

    test6

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get "inquiry" => "inquiry#index"              # 入力画面
-  post "inquiry/confirm" => "inquiry#confirm"   # 確認画面
-  post "inquiry/thanks" => "inquiry#thanks"     # 送信完了画面
+  get "inquiry" => "inquiry#index"
+  post "inquiry/confirm" => "inquiry#confirm"
+  post "inquiry/thanks" => "inquiry#thanks"
 
   get "inquiry/index"
   get "inquiry/confirm"
